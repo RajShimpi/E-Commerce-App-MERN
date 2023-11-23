1591b53b661b3f37c129cba4b9805eb57eb56a47
RajShimpi <rajshimpi8812@gmail.com>
2023-11-23 22:47:30 +0530

    Create some routes like get user delete user

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,6 +1,7 @@
 const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModel");
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+
 const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
     const findUser = await User.findOne({ email: email });
@@ -32,4 +33,46 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { createUser, loginUserCtrl };
+// Get all User
+
+const getAllUsers = asyncHandler(async (req, res) => {
+    try {
+        const users = await User.find();
+
+        // Send a successful response with the retrieved users
+        res.status(200).json({ success: true, data: users });
+    } catch (error) {
+        // Log the error for debugging purposes
+        console.error(error);
+
+        // Send an error response to the client
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+});
+
+//Get a single user 
+const getaUser = asyncHandler(async(req,res)=>{
+    const {id} =req.params;
+    try{
+        const getaUser =await User.findById(id);
+        res.json({
+            getaUser,
+        })
+    } catch(error){
+        throw new Error(error)
+    }
+})
+
+//Delete a user 
+const deleteaUser = asyncHandler(async(req,res)=>{
+    const {id} =req.params;
+    try{
+        const deleteaUser =await User.findByIdAndDelete(id);
+        res.json({
+            deleteaUser,
+        })
+    } catch(error){
+        throw new Error(error)
+    }
+})
+module.exports = { createUser, loginUserCtrl, getAllUsers, getaUser, deleteaUser };
